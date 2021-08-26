f42906f9183db78938e7cd8a5595f20db23950d6
Anurag Goel <anurag@render.com>
2021-08-26 14:11:10 -0700

    Make advertises routes and TS version configurable

--- start.sh
+++ start.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
+# Ignore Tailscale during port detection on Render
 if [[ ${RENDER_PORT_DETECTOR:-} != "true" ]]; then
   # shellcheck source=projectmgr/run-tailscale.sh
   . /home/render/run-tailscale.sh
 fi
 
+# Start a simple HTTP server to run this repo as a private service
 RESPONSE="HTTP/1.1 200 OK\r\nConnection: keep-alive\r\n\r\nOK}\r\n"
 while { echo -en "$RESPONSE"; } | nc -l 10000; do
   echo "================================================"
