09fea09b929a27d8ca9ee5c7285e3c3068fe9b6a
David Mauskop <david@render.com>
2021-08-30 00:30:26 -0700

    Move tailscale and tailscaled to /usr/bin

--- install-tailscale.sh
+++ install-tailscale.sh
@@ -3,6 +3,6 @@ set -x
 TAILSCALE_VERSION=${TAILSCALE_VERSION:-1.14.0}
 TS_FILE=tailscale_${TAILSCALE_VERSION}_amd64.tgz
 wget -q "https://pkgs.tailscale.com/stable/${TS_FILE}" && tar xzf "${TS_FILE}" --strip-components=1
-cp -r tailscale tailscaled /home/render/
+cp -r tailscale tailscaled /usr/bin/
 
 mkdir -p /var/run/tailscale /var/cache/tailscale /var/lib/tailscale
