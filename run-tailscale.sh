7af9a6edd9edb71baae6450d3e99e0940ebd96af
Anurag Goel <anurag@render.com>
2021-09-19 12:22:12 -0700

    Tailscale 1.14.3 waits for tailscaled

--- run-tailscale.sh
+++ run-tailscale.sh
@@ -3,10 +3,12 @@ if [[ ${TAILSCALE_AUTHKEY:-} ]]; then
   tailscaled -tun=userspace-networking -socks5-server=localhost:1055 &
 
   ADVERTISE_ROUTES=${ADVERTISE_ROUTES:-10.0.0.0/8}
-  until tailscale up --authkey="${TAILSCALE_AUTHKEY}" --hostname="${RENDER_SERVICE_NAME}" --advertise-routes="$ADVERTISE_ROUTES"; do
-    sleep 0.1
-  done
+  tailscale up --authkey="${TAILSCALE_AUTHKEY}" \
+    --hostname="${RENDER_SERVICE_NAME}" \
+    --advertise-routes="$ADVERTISE_ROUTES"
+
   export ALL_PROXY=socks5://localhost:1055/
+
   tailscale_ip=$(tailscale ip)
   echo "Tailscale is up at IP ${tailscale_ip}"
 fi
