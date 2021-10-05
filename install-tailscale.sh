737c455030ed8defadbf8a94f315d7f74362580a
John Beynon <john@render.com>
2021-10-05 14:39:32 +0100

    Bump to Tailscale 1.14.6

--- install-tailscale.sh
+++ install-tailscale.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -x
-TAILSCALE_VERSION=${TAILSCALE_VERSION:-1.14.3}
+TAILSCALE_VERSION=${TAILSCALE_VERSION:-1.14.6}
 TS_FILE=tailscale_${TAILSCALE_VERSION}_amd64.tgz
 wget -q "https://pkgs.tailscale.com/stable/${TS_FILE}" && tar xzf "${TS_FILE}" --strip-components=1
 cp -r tailscale tailscaled /usr/bin/
