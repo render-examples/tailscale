159c5c044d8f28bda13f41a9f9d2c76d27312675
Chris Castle <chris@render.com>
2021-10-27 14:12:38 -0400

    Tailscale version bump and fix tailscaled crashing (#7)
    * Bump Tailscale to 1.16.1
    * Fix tailscaled dying right after start

--- install-tailscale.sh
+++ install-tailscale.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -x
-TAILSCALE_VERSION=${TAILSCALE_VERSION:-1.14.3}
+TAILSCALE_VERSION=${TAILSCALE_VERSION:-1.16.1}
 TS_FILE=tailscale_${TAILSCALE_VERSION}_amd64.tgz
 wget -q "https://pkgs.tailscale.com/stable/${TS_FILE}" && tar xzf "${TS_FILE}" --strip-components=1
-cp -r tailscale tailscaled /usr/bin/
+cp -r tailscale tailscaled /render/
 
 mkdir -p /var/run/tailscale /var/cache/tailscale /var/lib/tailscale
--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Ignore Tailscale during port detection on Render
 if [[ ${RENDER_PORT_DETECTOR:-} != "true" ]]; then
-  . /home/render/run-tailscale.sh
+  . /render/run-tailscale.sh
 fi
 
 # Start a simple HTTP server to run this repo as a private service
