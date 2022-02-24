0281b48d402242f2b5d105026a20bbd69fbe37c8
Chris Castle <chris@crc.io>
2022-02-23 22:32:32 -0800

    Bump Tailscale to v1.22.0

--- install-tailscale.sh
+++ install-tailscale.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -x
-TAILSCALE_VERSION=${TAILSCALE_VERSION:-1.16.1}
+TAILSCALE_VERSION=${TAILSCALE_VERSION:-1.22.0}
 TS_FILE=tailscale_${TAILSCALE_VERSION}_amd64.tgz
 wget -q "https://pkgs.tailscale.com/stable/${TS_FILE}" && tar xzf "${TS_FILE}" --strip-components=1
 cp -r tailscale tailscaled /render/
