5bfc0ea00ca147dfe65577c19cd58ad5574151e0
davidmauskop <david@render.com>
2022-05-16 16:08:26 -0700

    Update start script to be more worker-like

--- run-tailscale.sh
+++ run-tailscale.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
-if [[ ${TAILSCALE_AUTHKEY:-} ]]; then
-  /render/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
 
-  ADVERTISE_ROUTES=${ADVERTISE_ROUTES:-10.0.0.0/8}
-  until /render/tailscale up --authkey="${TAILSCALE_AUTHKEY}" --hostname="${RENDER_SERVICE_NAME}" --advertise-routes="$ADVERTISE_ROUTES"; do
-    sleep 0.1
-  done
-  export ALL_PROXY=socks5://localhost:1055/
-  tailscale_ip=$(/render/tailscale ip)
-  echo "Tailscale is up at IP ${tailscale_ip}"
-fi
+/render/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
+PID=$!
+
+ADVERTISE_ROUTES=${ADVERTISE_ROUTES:-10.0.0.0/8}
+until /render/tailscale up --authkey="${TAILSCALE_AUTHKEY}" --hostname="${RENDER_SERVICE_NAME}" --advertise-routes="$ADVERTISE_ROUTES"; do
+  sleep 0.1
+done
+export ALL_PROXY=socks5://localhost:1055/
+tailscale_ip=$(/render/tailscale ip)
+echo "Tailscale is up at IP ${tailscale_ip}"
+
+wait ${PID}
