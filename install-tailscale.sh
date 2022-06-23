8414b0896a76c7f20ee54d6ca530e210919a67b1
Jeremy Georges-Filteau <jeremy.geo@gmail.com>
2022-06-23 19:51:22 +0200

    Update install-tailscale.sh

--- install-tailscale.sh
+++ install-tailscale.sh
@@ -2,14 +2,12 @@
 set -x
 TAILSCALE_VERSION=${TAILSCALE_VERSION:-1.22.0}
 TS_FILE=tailscale_${TAILSCALE_VERSION}_amd64.tgz
-
 echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.conf
 echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.conf
-sudo sysctl -p /etc/sysctl.conf
+sysctl -p /etc/sysctl.conf
 
-apt update && apt upgrade -y
-apt install curl
+wget -q "https://pkgs.tailscale.com/stable/${TS_FILE}" && tar xzf "${TS_FILE}" --strip-components=1
+cp -r tailscale tailscaled /render/
 
-curl -fsSL https://tailscale.com/install.sh | sh
+mkdir -p /var/run/tailscale /var/cache/tailscale /var/lib/tailscale
 
-tailscale up --authkey=${TAILSCALE_AUTHKEY} --advertise-routes=${ADVERTISE_ROUTES} --accept-routes
